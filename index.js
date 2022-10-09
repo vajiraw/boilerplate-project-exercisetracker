5f6f68d56cd9d66db3b7528c920a591208e98cd3
Vajira <vajirawke@gmail.com>
2022-10-09 12:41:55 +0530

    add get list of users function

--- index.js
+++ index.js
@@ -2,13 +2,51 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+
+const User = require('./user')
+const { default: mongoose } = require('mongoose')
+const user = require('./user')
+//const user = require('./user')
+
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.ipvtxd6.mongodb.net/?retryWrites=true&w=majority`)
+        .then(()=>{console.log('connected');})
+        .catch((err)=>{console.log(err);})
+
+
 
 app.use(cors())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
+app.post('/api/users',(req,res)=>{
+  let name = req.body.username
+  let user = new User({username:name})
+  user.save((err,data)=>{
+    if(err){
+      console.error(err);
+    }
+    console.log(data);
+  });  
+})
+
+
+
+app.get('/api/users',(req,res)=>{
+  user.find((err,data)=>{
+    //console.log(data);
+    res.send(data)
+  })
+})
 
 
 
