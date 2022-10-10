f036e0434084fc41f31cda42dfd45e77ceb90bea
Vajira <vajirawke@gmail.com>
2022-10-10 10:10:25 +0530

    updatee for exercise insert

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require('cors')
 require('dotenv').config()
 const bodyParser = require('body-parser')
 
-const User = require('./user')
+//const User = require('./user')
 const { default: mongoose } = require('mongoose')
-const user = require('./user')
+const { Schema } = mongoose;
+const {User,Exercise} = require('./user')
 //const user = require('./user')
 
 mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.ipvtxd6.mongodb.net/?retryWrites=true&w=majority`)
@@ -27,7 +28,8 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+// iinsert a  new user with user ame and generated default _id and response with
+// inserted record
 app.post('/api/users',(req,res)=>{
   let name = req.body.username
   let user = new User({username:name})
@@ -39,13 +41,68 @@ app.post('/api/users',(req,res)=>{
   });  
 })
 
-
-
+// this method retrives all reords
 app.get('/api/users',(req,res)=>{
   user.find((err,data)=>{
     //console.log(data);
     res.send(data)
   })
+})
+
+///api/users/:_id/exercises
+app.post('/api/users/:_id/exercises',(req,res)=>{
+  let {_id} = req.params
+  console.log("id :: "+_id);
+  let description = req.body.description
+  let duration = req.body.duration
+  let date = req.body.date
+  // if(req.body.date===""){
+  //     date =  new Date()
+  // }
+  req.body.date === ""?date = new Date(): date = req.body.date;    
+  //console.log(description,duration,date);
+
+  //let n = new Schema.Types.ObjectId(_id)
+  let exercise =  new Exercise(
+    {
+    'user' : _id,    
+    'description' : description,
+    'duration' : parseInt(duration),
+    'date' : new Date(date)
+  })
+  console.log(exercise);
+  let id = exercise._id;
+  exercise.save((err,data)=>{
+    if(err){console.log(err);}
+    id = data._id;
+    //res.send(data._id)
+
+
+    Exercise.
+    findById(id).
+    populate('user').
+    exec(function (err, exercise1) {
+    if (err) console.log(err);
+    console.log('The  is %s', exercise1);    
+    res.json({'username':exercise1.user.username,
+              'desdription':exercise.description,
+              'duration': exercise.duration,
+              'date' : exercise.date,
+              'id': id
+    });
+    
+
+
+  })
+
+
+
+  })
+
+
+  
+    
+  
 })
 
 
--- user.js
+++ user.js
@@ -1,4 +1,5 @@
 let mongoose = require('mongoose')
+const { Schema } = mongoose;
 
 let userSchema = mongoose.Schema({
     username: {type:String}
@@ -6,6 +7,17 @@ let userSchema = mongoose.Schema({
 
 let user = mongoose.model('User',userSchema)
 
-module.exports = user;
+//exercises 
+let exerciseSchema = mongoose.Schema({
+    userid:{type:Schema.ObjectId},
+    user: { type: Schema.ObjectId, ref: 'User' },
+    description: {type:String},
+    duration:Number,
+    date: Date
+})
+
+let exercise = mongoose.model('Exercise',exerciseSchema)
+
+module.exports = {User:user,Exercise:exercise};
 
 
